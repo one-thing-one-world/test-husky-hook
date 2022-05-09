78590a844531a41047c09947438cc9f17c7baf01
liujuncai <1769031928@qq.com>
2022-05-09 17:15:36 +0800

    tests: test some code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+function Some() {
+  return <div>hello world</div>
+}
 export default function App() {
-  return <div>App</div>
+  return (
+    <div>
+      <Some />
+    </div>
+  )
 }
