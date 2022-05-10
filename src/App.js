e4e145f8811f4efa292f0da5c890ca3dad814938
liujuncai <1769031928@qq.com>
2022-05-10 13:57:44 +0800

    test: test code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Some() {
   return <div>hello worldsss</div>
 }
 export default function App() {
+  const [testCode, setTestCode] = useState(false)
+  useEffect(() => {
+    setTestCode(true)
+    console.log(testCode)
+  }, [])
   return (
     <div>
       <Some />
